4e087ffed7331bd79f2b709e8bf8b2b6598cdf5b
Stephen Low <stphlow@amazon.com>
2021-01-09 13:43:45 -0500

    RSVP functionality

--- js/scripts.js
+++ js/scripts.js
@@ -194,23 +194,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Theresa & Stephen's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jul 30, 2021 13:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jul 31, 2021 01:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Deer Creek Banquet Facility, Ajax, Ontario',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Stephen Low at +1 718-290-7573."
         }
     });
 
@@ -303,7 +303,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyDqInXEWdIDAaRM5wEy26iFEJ2_qT2yr1cEw6RxWwisOdx4118/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
